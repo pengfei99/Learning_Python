7f371092e33017e523be25a20ccbb27b4cf3ff4c
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-04-10 13:50:20 +0200

    update

---
+++ learning_python/Lesson04_Module_Package/src/pkg/sub_pkg/mod6.py
@@ -0,0 +1,11 @@
+def show_global_var():
+    from learning_python.Lesson04_Module_Package.src.pkg import pkg_data
+    print(f"The global var imported from pkg: {pkg_data}")
+
+
+def mod1_func():
+    print('This is [mod1] function()')
+
+
+class Mod1Class:
+    pass
